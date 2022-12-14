3b45bfba414185b1b93636e162a0c8517f9f7642
siakro <siakro@users.noreply.github.com>
2022-12-14 10:51:52 +0100

    feat(pi-log): op prints list joined with whitespace

--- packages/plugins/pi-log/dist/cjs/ops/info.js
+++ packages/plugins/pi-log/dist/cjs/ops/info.js
@@ -4,15 +4,18 @@ var _core = require("@dogmalang/core");
 module.exports = exports = {
   ["desc"]: "Prints a value.",
   ["title"]: "log: print a value",
-  ["fun"]: run
+  ["fun"]: handle
 };
-function run(ctx) {
+function handle(ctx) {
   /* c8 ignore next */_core.dogma.expect("ctx", ctx, _core.map);
   let {
     params,
     log
   } = ctx;
   {
+    if (_core.dogma.is(params, _core.list)) {
+      params = params.join(" ");
+    }
     log(params);
   }
 }
