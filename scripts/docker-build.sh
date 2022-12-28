6605888b160b3a787022cc06a6f8fce0daa15d11
siakro <siakro@users.noreply.github.com>
2022-12-28 09:18:44 +0100

    chore(docker): configured for publishing major.minor version too

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,6 +2,7 @@
 
 # (1) get gattuso version
 version=$(npm pkg --workspace @akromio/gattuso get version | grep gattuso | grep -o -E "[[:digit:]]+.[[:digit:]]+.[[:digit:]]")
+mmVersion=$(npm pkg --workspace @akromio/gattuso get version | grep gattuso | grep -o -E "[[:digit:]]+.[[:digit:]]+")
 
 # (2) build docker image
-sudo docker build -t akromio/gattuso:latest -t akromio/gattuso:$version --build-arg version=$version docker/alpine/
+sudo docker build -t akromio/gattuso:latest -t akromio/gattuso:$mmVersion -t akromio/gattuso:$version --build-arg version=$version docker/alpine/
