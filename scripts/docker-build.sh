4345ac464da4d7174464ebe263675e88218920c2
Raúl G. González <105838447+siakro@users.noreply.github.com>
2022-12-30 09:22:06 +0100

    feat(docker): added carboni image (#168)

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# (1) get gattuso version
+# (1) get version
 version=$(npm pkg --workspace @akromio/gattuso get version | grep gattuso | grep -o -E "[[:digit:]]+.[[:digit:]]+.[[:digit:]]")
 mmVersion=$(npm pkg --workspace @akromio/gattuso get version | grep gattuso | grep -o -E "[[:digit:]]+.[[:digit:]]+")
 
 # (2) build docker image
 sudo docker build -t akromio/gattuso:latest -t akromio/gattuso:$mmVersion -t akromio/gattuso:$version --build-arg version=$version docker/alpine/
+sudo docker build -t akromio/carboni:latest -t akromio/carboni:$mmVersion -t akromio/carboni:$version --build-arg version=$version docker/alpine/
--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# (1) get gattuso version
+# (1) get version
 version=$(npm pkg --workspace @akromio/gattuso get version | grep gattuso | grep -o -E "[[:digit:]]+.[[:digit:]]+.[[:digit:]]")
+mmVersion=$(npm pkg --workspace @akromio/gattuso get version | grep gattuso | grep -o -E "[[:digit:]]+.[[:digit:]]+")
 
-# (2) build docker images
+# (2) publish docker images
 sudo docker push akromio/gattuso:latest
 sudo docker push akromio/gattuso:$version
+sudo docker push akromio/gattuso:$nmVersion
+
+sudo docker push akromio/carboni:latest
+sudo docker push akromio/carboni:$version
+sudo docker push akromio/carboni:$nmVersion
